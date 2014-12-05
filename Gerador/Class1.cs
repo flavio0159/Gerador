55195429bf5fd965041ade2ff527c0f1aa15cb97
flavio0159 <flavioo-ss@hotmail.com>
2014-12-05 02:39:22 -0200

    Zac

---
+++ Gerador/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gerador
+{
+    class Class1
+    {
+        int a;
+    }
+}
